f84b1c2f4b4cc05c756e5abfd22bde4c25f8acbc
Ankit9835 <ankydchrismatic@gmail.com>
2023-01-23 17:55:38 +0530

    react app setup

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+         <Route index element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>  
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
